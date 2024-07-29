000350fb19fb9bf95f54e0eacf8ab0d9f67ba380
unknown <akhang147@gmail.com>
2024-07-29 17:45:12 +0700

    Change Swap

--- test/swap2.spec.js
+++ test/swap2.spec.js
@@ -9,7 +9,7 @@ function sleep(ms) {
   let driver;
   try {
     driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("https://app-core-client.vercel.app/");
+    await driver.get("https://solx-stg.blocktrend.xyz/");
     await sleep(10000);
     await driver.manage().setTimeouts({ implicit: 20000 });
     await driver.manage().window().maximize();
@@ -30,24 +30,32 @@ function sleep(ms) {
     await newWallet.click();
 
     let initialWindowHandles = await driver.getAllWindowHandles();
-    await sleep(2000);
+    await sleep(5000)
 
-    await driver.wait(async () => {
-      let handles = await driver.getAllWindowHandles();
-      return handles.length > initialWindowHandles.length;
-    }, 30000);
 
+    let newWindowHandle;
     let allWindowHandles = await driver.getAllWindowHandles();
-    let newWindowHandle = allWindowHandles.find(handle => !initialWindowHandles.includes(handle));
-    await sleep(5000);
+
+    for (let handle of allWindowHandles) {
+        await driver.switchTo().window(handle);
+        let url = await driver.getCurrentUrl();
+        if (url === 'https://solflare.com/onboard') {
+            newWindowHandle = handle;
+            break;
+        }
+    }
+
+    if (!newWindowHandle) {
+        throw new Error('Không tìm thấy cửa sổ mới với URL mong muốn');
+    }
 
     await driver.switchTo().window(newWindowHandle);
     await sleep(5000);
 
     let alreadyWallet = driver.findElement(By.xpath('//*[@id="root"]/div/div[2]/div/div[2]/div[2]/button'));
-    await driver.executeScript("arguments[0].scrollIntoView(true);", alreadyWallet);
-    await sleep(2000);
+    // await driver.executeScript("arguments[0].scrollIntoView(true);", alreadyWallet);
     await driver.executeScript("arguments[0].click();", alreadyWallet);
+    await sleep(2000);
 
     let mnemonicWords = [
       "obtain", "lobster", "taste", "debate",
@@ -96,20 +104,51 @@ function sleep(ms) {
     let newWallet1 = await driver.findElement(By.xpath('//*[@id="connect-web-button"]'));
     await newWallet1.click();
 
+    // await driver.wait(async () => {
+    //   let handles = await driver.getAllWindowHandles();
+    //   return handles.length > initialWindowHandles.length;
+    // }, 10000);
+
+    // let newWindowHandle1;
+    //     let allWindowHandles1 = await driver.getAllWindowHandles();
+    //     await sleep(5000);
+
+    //     for (let handle of allWindowHandles1) {
+    //         await driver.switchTo().window(handle);
+    //         let url = await driver.getCurrentUrl();
+    //         if (url === 'https://solflare.com/provider#origin=https%3A%2F%2Fapp-core-client.vercel.app&network=mainnet-beta') {
+    //             newWindowHandle1 = handle;
+    //             break;
+    //         }
+    //     }
+
+    //     if (!newWindowHandle1) {
+    //         throw new Error('Không tìm thấy cửa sổ mới với URL mong muốn lần thứ hai');
+    //     }
+
+    // await driver.switchTo().window(newWindowHandle1);
+
     await driver.wait(async () => {
       let handles = await driver.getAllWindowHandles();
       return handles.length > initialWindowHandles.length;
-    }, 10000);
+    }, 30000);
 
+    // Lấy lại tất cả các handle của cửa sổ sau khi cửa sổ mới mở
     let allWindowHandles1 = await driver.getAllWindowHandles();
-    let newWindowHandle1 = allWindowHandles1.find(handle => !initialWindowHandles.includes(handle));
+
+    // Tìm handle của cửa sổ mới (khác với các handle ban đầu)
+    let newWindowHandle1 = allWindowHandles1.find(
+      (handle) => !initialWindowHandles.includes(handle)
+    );
+
+    // Chuyển sang cửa sổ mới
     await driver.switchTo().window(newWindowHandle1);
+
 
     let connectWallet = await driver.findElement(By.xpath('/html/body/div[2]/div[2]/div/div[3]/div/button[2]'));
     await connectWallet.click();
-
     await driver.switchTo().window(originalWindow);
-    await sleep(10000);
+    await sleep(2000);
 
     let boostButton = await driver.findElement(By.xpath('//*[@id="home"]/div[2]/div/div[3]/div[2]/div/div[1]/button[2]'));
     await driver.executeScript("arguments[0].scrollIntoView(true);", boostButton);
@@ -122,10 +161,15 @@ function sleep(ms) {
     await swapSection.click();
     await sleep(4000);
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 3; i++) {
       await sleep(2000);
       let inputButton = await driver.findElement(By.xpath('//*[@id="input_mint"]'));
       await inputButton.sendKeys("10");
+
+      await sleep(2000);
+
+      // let swapButton = await driver.findElement(By.xpath('//*[@id="home"]/div[2]/div/div[3]/div[2]/div/div[2]/div[2]/div/button'));
+      // await swapButton.click();
 
       let swapButton = await driver.findElement(By.xpath('//*[@id="home"]/div[2]/div/div[3]/div[2]/div/div[2]/div[2]/div/button'));
       await driver.executeScript("arguments[0].click();", swapButton);
@@ -133,17 +177,27 @@ function sleep(ms) {
       await driver.wait(async () => {
         let handles = await driver.getAllWindowHandles();
         return handles.length > initialWindowHandles.length;
-      }, 10000);
-
+      }, 21000);
+      
+      // Lấy lại tất cả các handle của cửa sổ sau khi cửa sổ mới mở
       let allWindowHandles2 = await driver.getAllWindowHandles();
-      let newWindowHandle2 = allWindowHandles2.find(handle => !initialWindowHandles.includes(handle));
+      
+      // Tìm handle của cửa sổ mới (khác với các handle ban đầu)
+      let newWindowHandle2 = allWindowHandles2.find(
+        (handle) => !initialWindowHandles.includes(handle)
+      );
+      
+      // Chuyển sang cửa sổ mới
       await driver.switchTo().window(newWindowHandle2);
-
-      let approveButton = await driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/button[2]"));
+      
+      let approveButton = await driver.findElement(
+        By.xpath(
+          "/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/button[2]"
+        )
+      );
       await approveButton.click();
-
+      
       await driver.switchTo().window(originalWindow);
-      await sleep(2000);
     }
   } catch (e) {
     console.error("Đã xảy ra lỗi:", e);
