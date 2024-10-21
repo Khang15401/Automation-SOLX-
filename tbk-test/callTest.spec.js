a6ee75a5c325a3beedaba96ffaa2b9dcaa0ae3f9
Khang <akhang147@gmail.com>
2024-10-21 17:22:33 +0700

    change token access

--- tbk-test/callTest.spec.js
+++ tbk-test/callTest.spec.js
@@ -12,16 +12,18 @@ function sleep(ms) {
 
   const url = "https://tbkx-core-be-sandbox.up.railway.app/user/connectBE";
   const token = "YWRtaW46NzNBRENBQTRGRTNDNUNCMzE2OTIxNjI1RjE3OUQ==";
+  const access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiI3MTM1NDc2OTUzIiwidHlwZSI6InRlbGVncmFtIiwidXNlcl9pZCI6ImFjN2U1ZmJhLTI2OTUtNDUzMS05YmFmLTJjYzk3MDRmNDc2YSIsImlhdCI6MTcyOTUwNTQzMiwiZXhwIjoxNzI5NTA5MDMyfQ.eQ-yamvzq-Md-IhP3YyMww6TGA9cYAzbTCsAd172Kv4'
+  const access_token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiI2MDA3NzQ3OTM5IiwidHlwZSI6InRlbGVncmFtIiwidXNlcl9pZCI6IjNhMDE0NjVmLTg2M2EtNDBhYS04OTc3LTdjNDUzZTY3NDcyNyIsImlhdCI6MTcyOTUwNTM0MywiZXhwIjoxNzI5NTA4OTQzfQ.Om_OYk0AqrA6YrRwq4sqhJyB58VQXfToQ3aVAM0mAzw'
 
   const telegram_id = "6007747939";
   const telegram_id1 = "7135476953";
 
-  const userId = "d37b6c5f-b96c-4632-9a32-84dce3191ff3";
-  const userId1 = 'cfe6feaf-687b-47cb-bde9-94069031c252';
-  const userId2 = '4af8519f-1f53-45e2-9f68-ecffb6baef82';
+  // const userId = 'ac7e5fba-2695-4531-9baf-2cc9704f476a';
+  // const userId1 = '3a01465f-863a-40aa-8977-7c453e674727';
+  // const userId2 = '4af8519f-1f53-45e2-9f68-ecffb6baef82';
   const boostDataUrl =
     "https://tbkx-core-be-sandbox.up.railway.app/boost/boost-data";
-  const preSaleUrl = "https://tbkx-core-be-sandbox.up.railway.app/boost/fake";
+  const preSaleUrl = "https://tbkx-core-be-sandbox.up.railway.app/boost/v2/claim-reward";
   const pocketUrl =
     "https://pocketbase-sandbox-6bd6.up.railway.app/api/admins/auth-with-password";
   const referralUrl = 'https://tbkx-core-be-sandbox.up.railway.app/affiliates/referral-data';
@@ -38,43 +40,62 @@ function sleep(ms) {
   const signatureRandom = generateRandomSignature(64);
   console.log("Random Signature:", signatureRandom);
 
-  // const signatureRandom ='ocNjkaYBIBhL9qzqgxjd9La8vP2LVPoufmxmIEvL6qQvV8vmeuWnZB64q5RVJJxt';
+  const bocHashRandom = generateRandomSignature(64);
+  console.log("bocHash Signature:", bocHashRandom);
 
+
+  // Function random blocktime
+  function generateRandomBlockTime() {
+    const currentTime = Math.floor(Date.now() / 1000);
+  
+    const fiveDaysInSeconds = 5 * 24 * 60 * 60;
+    const startTime = currentTime - fiveDaysInSeconds;
+  
+    const randomBlockTime = Math.floor(
+      Math.random() * (currentTime - startTime + 1) + startTime
+    );
+  
+    return randomBlockTime;
+  }
+  
+  const randomTime = generateRandomBlockTime();
+  console.log("Random Block Time:", randomTime);
+  
 
   const pocketFilterCollection = `https://pocketbase-sandbox-6bd6.up.railway.app/api/collections/tli6s8xff4hw88r/records?page=1&perPage=40&sort=-amount_principal&skipTotal=1&filter=hash_fee%20%3D%20%22${signatureRandom}%22&expand=&fields=`;
   // console.log(pocketFilterCollection);
 
   try {
-    const response = await axios.post(
-      url,
-      { telegram_id: telegram_id },
-      {
-        headers: {
-          Authorization: `Basic ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const access_token = response.data.access_token;
+    // const response = await axios.post(
+    //   url,
+    //   { telegram_id: telegram_id },
+    //   {
+    //     headers: {
+    //       Authorization: `Basic ${token}`,
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
+    // const access_token = response.data.access_token;
     // console.log(access_token);
 
-    const response1 = await axios.post(url, { telegram_id: telegram_id1 }, {
-      headers: {
-        'Authorization': `Basic ${token}`,
-        'Content-Type': 'application/json',
-      }
-    });
-    const access_token1 = (response1.data.access_token);
-    console.log(access_token1);
+    // const response1 = await axios.post(url, { telegram_id: telegram_id1 }, {
+    //   headers: {
+    //     'Authorization': `Basic ${token}`,
+    //     'Content-Type': 'application/json',
+    //   }
+    // });
+    // const access_token1 = (response1.data.access_token);
+    // console.log(access_token1);
 
     const getSaleGroup = await axios.get(referralUrl, {
       headers: {
-        'Authorization': `Bearer ${access_token1}`,
+        'Authorization': `Bearer ${access_token}`,
         'Content-Type': "application/json",
       }
     });
     const Group_Sale_Now = getSaleGroup.data.result.data.group_sales;
-    // console.log('Group_Sale_Now: ', getSaleGroup.data.result.data.group_sales);
+    console.log('Group_Sale_Now: ', getSaleGroup.data.result.data.group_sales);
 
 
 
@@ -86,21 +107,39 @@ function sleep(ms) {
 
     const preSale = await axios.post(preSaleUrl,
       {
-        "JettonTransfer": {
-          "contractAddress": "EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs",
-          "walletFrom": "EQDKXNR2xopctqwjKhN6AT9WqGdOK0XB2CveoTYEc4oMRVG7",
-          "walletTo": "EQAvUOECK9bkx8Wfgu7cVJcN6WrvAl9SFlW9JLb9jTWB8nSg",
-          "amount": 0.01
+        "TonTransferFee": {
+          "walletFrom": "",
+          "walletTo": "",
+          "amount": 0,
+          "type": ""
         },
-        "feeNetwork": 0.008407052,
-        "blockTime": 1726822698,
+        "TonTransfer": {},
+        "JettonTransfer": {
+          "walletFrom": "EQAOTN6K6uXoz3OW3w2ramezDuIDnGX7YNDuVKIqmfgj3V0M",
+          "walletTo": "EQAvUOECK9bkx8Wfgu7cVJcN6WrvAl9SFlW9JLb9jTWB8nSg",
+          "amount": 0.03,
+          "comment": "3a01465f-863a-40aa-8977-7c453e674727",
+          "contractAddress": "EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs"
+        },
+        "TonTransferTemp": [],
+        "JettonTransferTemp": [
+          {
+            "walletFrom": "EQAOTN6K6uXoz3OW3w2ramezDuIDnGX7YNDuVKIqmfgj3V0M",
+            "walletTo": "EQAvUOECK9bkx8Wfgu7cVJcN6WrvAl9SFlW9JLb9jTWB8nSg",
+            "amount": 0.03,
+            "comment": "3a01465f-863a-40aa-8977-7c453e674727",
+            "contractAddress": "EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs"
+          }
+        ],
+        "feeNetwork": 0.00712322,
+        "blockTime": `${randomTime}`,
         "signature": `${signatureRandom}`,
         "type": "boost",
-        "typeInternal": "j:false--u:bafac4df-51b7-4301-aa91-d5018fbacf61--t:boost--j:true--a:false--n:1e-8",
-        "bocHash": "b21df0bde0b12eb82fd0a681b5f1490dc2827029e7fd3a9eec7ccf7321711449",
+        "typeInternal": "3a01465f-863a-40aa-8977-7c453e674727",
+        "bocHash": `${bocHashRandom}`,
         "publicKey": "",
         "isJetton": "true",
-        "userId": `${userId2}`,
+        "userId": "3a01465f-863a-40aa-8977-7c453e674727"
       },
       {
 
@@ -111,7 +150,7 @@ function sleep(ms) {
       }
     );
 
-    // // console.log('Pre-sale API:', preSale.data);
+    // console.log('Pre-sale API:', preSale.data);
 
 
     await sleep(25000);
@@ -122,21 +161,22 @@ function sleep(ms) {
         "Content-Type": "application/json",
       },
     });
-    // const itemsArray = findTransacion.data.items;
-    // fs.writeFileSync('resultApiAuto.json', JSON.stringify(itemsArray, null, 2), 'utf-8');
+    console.log(findTransacion.data);
+    const itemsArray = findTransacion.data.items;
+    fs.writeFileSync('resultApiAuto.json', JSON.stringify(itemsArray, null, 2), 'utf-8');
 
-    // const boostData = await axios.get(boostDataUrl, {
-    //   headers: {
-    //     'Authorization': `Bearer ${access_token}`,
-    //     'Content-Type': 'application/json',
-    //   }
-    // });
+    const boostData = await axios.get(boostDataUrl, {
+      headers: {
+        'Authorization': `Bearer ${access_token}`,
+        'Content-Type': 'application/json',
+      }
+    });
     // console.log(boostData.data);
 
     await sleep(10000);
     const getSaleGroupAfter = await axios.get(referralUrl, {
       headers: {
-        'Authorization': `Bearer ${access_token1}`,
+        'Authorization': `Bearer ${access_token}`,
         'Content-Type': "application/json",
       }
     });
@@ -146,7 +186,6 @@ function sleep(ms) {
     console.log('Group Sale After: ',Group_Sale_After);
     console.log("=============================================================================================================================================================================================END=TEST=============================================================================================================================================================");
   } catch (error) {
-    // In ra lỗi nếu có
     console.error("Error occurred:", error);
   }
 })();
